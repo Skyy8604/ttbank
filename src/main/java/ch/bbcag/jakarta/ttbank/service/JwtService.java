628d312aba61c4d38990654ff6f3045651034add
sky8604 <skyelar.m@urer.ch>
2021-06-11 11:49:01 +0200

    added JwtService

---
+++ src/main/java/ch/bbcag/jakarta/ttbank/service/JwtService.java
@@ -0,0 +1,43 @@
+package ch.bbcag.jakarta.ttbank.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+
+public class JwtService {
+	private final String ISSUER = "gameXchange";
+	private final String SIGNING_KEY = "secret";
+
+	public String generateJwt(String subject) {
+		String token;
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(SIGNING_KEY);
+			token = JWT.create()
+					.withIssuer(ISSUER)
+					.withClaim("sub", subject)
+					.withIssuedAt(new Date())
+					.withExpiresAt(new Date(new Date().getTime() + 31154000000L))
+					.sign(algorithm);
+		} catch (JWTCreationException ex) {
+			throw ex;
+		}
+		return token;
+	}
+
+	public DecodedJWT verifyToken(String token) {
+		try {
+			Algorithm algorithm = Algorithm.HMAC256(SIGNING_KEY);
+			JWTVerifier verifier = JWT.require(algorithm).withIssuer(ISSUER).build();
+			DecodedJWT jwt = verifier.verify(token);
+			return jwt;
+		} catch (JWTVerificationException exception) {
+			throw exception;
+		}
+	}
+}
+
